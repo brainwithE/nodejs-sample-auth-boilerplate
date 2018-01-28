97061e01d8cfb8385768fba152a39494da3ec579
brainwithE <haylyzh@gmail.com>
2018-01-28 11:59:52 +0800

    Init boilerplate for sample auth using nodejs/express

---
+++ services/passport.js
@@ -0,0 +1,53 @@
+const passport = require('passport');
+const User = require('../models/user');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local strategy
+const localOptions = { usernameField: 'email'};
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  // Verify email and password, call done with the user if correct email and password
+  // else, call done with false
+
+  User.findOne({ email: email }, function(err, user) {
+    if (err) { return done(err); }
+    if (!user) { return done(null, false); }
+
+    // compare passwords - is password == to user.password?
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err); }
+      if (!isMatch) { return done(null, false); }
+
+      return done(null, user);
+    });
+  });
+});
+
+
+// Setup options for JWT strategy
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+};
+
+// Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) { //payload: decoded jwt token
+  // See if the user id in the payload exists in our database 
+  // If yes, call 'done' with that user. Otherwise, call done without a user object
+
+  User.findById(payload.sub, function(err, user) {
+    if (err) { return done(err, false); }
+
+    if (user) {
+      done(null, user)
+    } else {
+      done(null, false)
+    }
+  });
+});
+
+// Tell passport to use strategy
+passport.use(jwtLogin);
+passport.use(localLogin);
